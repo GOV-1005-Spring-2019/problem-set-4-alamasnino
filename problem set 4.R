34984f69c8288c73137ee11f37e81ed4bb6edef1
lamasnino <andrea.lamas-nino@thecrimson.com>
2019-02-28 09:51:34 -0500

    Current progress on Problem 2

--- problem set 4.R
+++ problem set 4.R
@@ -9,7 +9,7 @@ library(gt)
 
 ## Load data.
 elections <- read_csv("ps_4_elections-poll-nc09-3.csv") %>% 
-  filter(response%in%c("Dem","Rep","Und"))
+  filter(response%in%c("Dem","Rep","Und"), educ4!="[DO NOT READ] Refused",gender!= "[DO NOT READ] Refused", race_eth!="[DO NOT READ] Refused", file_race_black!="[DO NOT READ] Refused")
 
 ############################################### Problem 1
 
@@ -67,4 +67,43 @@ first_dem_response<-elections %>%
   summarize(first_dem_response=min(timestamp))
 first_rep_response-first_dem_response
 
+################################################################################## Problem 2
+ elections%>% 
+  filter(!is.na(race_eth),!is.na(response)) %>% 
+  #mutate(new_race_eth=as.character(race_eth)) %>% 
+  #select(response, new_race_eth,final_weight) %>% 
+  #select(new_race_eth)
+  group_by(race_eth, response) %>% 
+  # Use sum(weight_var) in place of n().*
+  summarize(total = sum(final_weight)) %>%   
+  filter(race_eth!= "[DO NOT READ] Don't know/Refused") %>% 
+  spread(key =  response, value = total) %>% 
+  mutate(all = Dem + Rep + Und) %>% 
+  mutate(Dem = Dem / all) %>% 
+  mutate(Rep = Rep / all) %>% 
+  mutate(Und = Und / all) %>% 
+  select(-all) %>% 
+  ungroup() %>% 
+  gt() %>% 
+  #race_eth=mutate(race_eth=fct_relevel(race_eth,c("White", "Black", "Hispanic", "Asian", "Other")))
+  tab_header(
+    title = "Polling Results in North Carolina 9th Congressional District") %>% 
+  cols_label(
+    new_race_eth = "Race", 
+    Dem = "DEM.",
+    Rep = "REP.",
+    Und = "UND."
+  ) %>%
+  tab_source_note = "Source: file://localhost/Users/andrealamasino/Desktop/Gov%201005/Problem%20Sets/problem-set-4-alamasnino/ps_4_elections-poll-nc09-3.csv" %>%  
 
+  
+  fmt_percent(columns = vars(Dem, Rep, Und),
+              decimals = 0) %>% 
+  row_group_order(c("White", "Black", "Hispanic", "Asian", "Other"))
+  
+  # This little pipe is that incantation to take this pretty table, turn it
+  # into html, and send it to the md file we are creating. Future versions of
+  # gt will probably have a better way of doing this. Indeed, does anyone know
+  # of one?
+  
+  #as_raw_html() %>% as.character() %>% cat()
